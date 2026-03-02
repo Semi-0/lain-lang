ae65c652342f3763dd48be16ba5615eac89d281a
PRim <longpp17@163.com>
2026-03-02 21:31:44 +0100

    removed core concept

--- src/grpc/card/index.ts
+++ src/grpc/card/index.ts
@@ -4,3 +4,10 @@
 export * from "./card_api.js";
 export type { CardDescription } from "./schema.js";
 export { construct_card_description, is_card_description } from "./schema.js";
+export {
+  minimal_vega_lite_spec,
+  vega_lite_spec_from_graph,
+  type VegaLiteSpec,
+  type GraphNode,
+  type GraphLink,
+} from "./vega_spec.js";
