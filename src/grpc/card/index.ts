a806083d039881db92569d0881435c2c6abbfef7
PRim <longpp17@163.com>
2026-03-03 19:14:48 +0100

    graph traverse reactive

--- src/grpc/card/index.ts
+++ src/grpc/card/index.ts
@@ -7,7 +7,9 @@ export { construct_card_description, is_card_description } from "./schema.js";
 export {
   minimal_vega_lite_spec,
   vega_lite_spec_from_graph,
+  vega_spec_from_graph,
   type VegaLiteSpec,
+  type VegaSpec,
   type GraphNode,
   type GraphLink,
 } from "./vega_spec.js";
